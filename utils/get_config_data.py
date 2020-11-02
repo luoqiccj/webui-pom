daa596f530c340c008174fae3198d8f096e9d53d
luoqiccj <luoqiccj@163.com>
2020-11-02 11:32:36 +0800

    写ini文件

--- utils/get_config_data.py
+++ utils/get_config_data.py
@@ -10,10 +10,12 @@ class GetConfigData:
 
         #未传入filepath参数，则默认赋值当前项目工作目录下的ConfigFile/config.ini
         if filepath==None:
-            filepath= os.path.join(pj_dir,"ConfigFile","config.ini")
+            self.filepath= os.path.join(pj_dir,"ConfigFile","config.ini")
             # print("__init__ filepath = %s" %filepath)
+        else:
+            self.filepath=filepath
         #读取指定路径的ini文件
-        self.cp.read(filepath)
+        self.cp.read(self.filepath)
 
     #获取当前项目工作目录
     def get_pj_dir(self):
@@ -27,8 +29,29 @@ class GetConfigData:
         # print("get_data data = %s" %data)
         return data
 
+    #新增一个section
+    def add_setion(self,section):
+        print("self.cp.has_section(section) = %s" %self.cp.has_section(section))
+        if not self.cp.has_section(section):
+            self.cp.add_section(section)
+
+    #section里面新增key和value
+    def add_option(self,section,option,value):
+        print("self.cp.has_option(section,option) = %s" % self.cp.has_option(section,option))
+        if  self.cp.has_option(section,option):
+            self.cp.remove_option(section, option)
+        self.cp.set(section, option, value)
+
+    #写ini文件
+    def write_ini(self):
+        fp = open(self.filepath,"w")
+        self.cp.write(fp)
+        fp.close()
+
 if __name__ == '__main__':
     cf = GetConfigData()
     data = cf.get_data("test_env","url")
+    cf.add_setion("test")
     print(data)
+
 
